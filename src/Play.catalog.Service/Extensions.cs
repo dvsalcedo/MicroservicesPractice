d23ca29bcae50e862c91a45a0809b9c996129b03
dvsalcedo <dvsalcedov@gmail.com>
2023-01-19 19:45:44 -0500

    Implemented async methods in Items Controller

---
+++ src/Play.catalog.Service/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Play.catalog.Service.Entities;
+using Play.Catalog.Service.Dtos;
+
+namespace Play.catalog.Service
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+        }
+    }
+}
